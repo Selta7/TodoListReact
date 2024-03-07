1132107c1d75a9ab1b39728593adba36c5c9bc69
Selta7 <132694122+Selta7@users.noreply.github.com>
2024-03-07 15:36:21 +0000

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoList from "./todolist";
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+
+//create your first component
+const Home = () => {
+	return (
+		<TodoList />
+	);
+};
+
+export default Home;
